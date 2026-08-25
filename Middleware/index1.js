9cd8b2a0ede2b0ae3377058da283a1b1dbd705cc
Noufan577 <noufankufi@gmail.com>
2026-08-25 13:44:21 +0530

    just learnign about middlewires for bacned using espress

---
+++ Middleware/index1.js
@@ -0,0 +1,15 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+res.sendFile(__dirname + "/public/index.html");
+});
