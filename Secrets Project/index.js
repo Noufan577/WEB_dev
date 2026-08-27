3be94b0586c9d60ea6a3efb143c727c2f981a150
Noufan577 <noufankufi@gmail.com>
2026-08-28 01:38:39 +0530

    learnt about axios

--- Secrets Project/index.js
+++ Secrets Project/index.js
@@ -1,16 +1,24 @@
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
+import express from "express";
+import axios from "axios";
 
-app.post("/check", (req, res) => {
-  if (userIsAuthorised) {
-    res.sendFile(__dirname + "/public/secret.html");
-  } else {
-    res.sendFile(__dirname + "/public/index.html");
-    //Alternatively res.redirect("/");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get("https://secrets-api.appbrewery.com/random");
+    res.render("index.ejs", {
+      secret: result.data.secret,
+      user: result.data.username,
+    });
+  } catch (error) {
+    console.log(error.response.data);
+    res.status(500);
   }
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
