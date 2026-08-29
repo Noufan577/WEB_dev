1dacde1c0178fb330e1a65d15869a505dbdfec68
Noufan577 <noufankufi@gmail.com>
2026-08-29 17:12:03 +0530

    react and secret project

---
+++ REACT/keeper-app-part-1-challenge/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Note(){
+    return (
+        <div className="note">
+            <h1>This is the note tittle</h1>
+            <p>This is the note content</p>
+        </div>
+    );
+}
+
+export default Note;
